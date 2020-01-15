e620c742d441fc1c51078e764616d85797c6ddb2
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 20:07:18 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 20:05
+ *Task: Go tour
+ */
+
+package main
+
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println(add(9, 9))
+}
