12f45439e99f6cdf62760c9a8c3c584b3cb1012b
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 21:32:15 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 21:31
+ *Task: Go tour
+ */
+
+package main
+
+import "fmt"
+
+//main func
+func main() {
+	a := 55
+	b := "b"
+	c := "Text"
+	d := false
+
+	fmt.Printf("a: Type %8T  Value %8v\n\n", a, a)
+	fmt.Printf("b: Type %8T  Value %8v\n\n", b, b)
+	fmt.Printf("c: Type %8T  Value %8v\n\n", c, c)
+	fmt.Printf("d: Type %8T  Value %8v\n\n", d, d)
+}
