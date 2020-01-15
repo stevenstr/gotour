4e3d375b4d12c4984c5d73fcc73ad7393ab1fb12
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 20:35:13 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 20:05
+ *Task: Go tour
+ */
+
+package main
+
+import "fmt"
+
+var a, b, c = 12, "sec", 5.4
+
+//main func
+func main() {
+	var x, y int = 2, 5
+	fmt.Println(a, b, c)
+	fmt.Println(x, y)
+}
