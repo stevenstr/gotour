b77584d4a65f772d868ce84b11a1c9a589b7acc9
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 20:10:08 +0300

    Initial commit

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ package main
 
 import "fmt"
 
-func add(a, b int) int {
-	return a + b
+func swap(a, b string) (x, y string) {
+	return b, a
 }
 
 func main() {
-	fmt.Println(add(9, 9))
+	var a = "hello"
+	var b = "world"
+
+	fmt.Println(swap(a, b))
 }
