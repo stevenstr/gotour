c225816209a339c75756ca127a7ad8946170b163
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 22:48:20 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 22:48
+ *Task: Go tour
+ */
+
+package main
+
+import "fmt"
+
+//main func
+func main() {
+	for {
+		fmt.Println("forever!")
+	}
+}
