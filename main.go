4a7227f4c1294e64ac8432479a53f2bc7df96431
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 21:01:30 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 21:01
+ *Task: Go tour
+ */
+
+package main
+
+import "fmt"
+
+//main func
+func main() {
+
+	var a int
+	var b float32
+	var bb float64
+	var bl bool
+	var txt string
+
+	fmt.Printf("a: type %T   value %v\n\n", a, a)
+	fmt.Printf("b: type %T   value %v\n\n", b, b)
+	fmt.Printf("bb: type %T   value %v\n\n", bb, bb)
+	fmt.Printf("bl: type %T   value %v\n\n", bl, bl)
+	fmt.Printf("txt: type %T   value %v\n\n", txt, txt)
+}
