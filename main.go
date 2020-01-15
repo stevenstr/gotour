b049f1e34633a56ee0301d518d8fa21f74aa3eb3
Ste Zha <stephenzhavrid@gmail.com>
2020-01-15 20:55:59 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+/*
+ *Author: Stefan
+ *Date: 01/15/2020
+ *Last changes: 01/15/2020 20:05
+ *Task: Go tour
+ */
+
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+//main func
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
